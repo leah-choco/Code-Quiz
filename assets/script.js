890de68de4c6b32b2787bc3346e8b8ffcfe4f2bb
Your Name <you@example.com>
2022-08-24 12:42:35 -0500

    Writing the majoritity of the JS

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,96 @@
-const startButton = document.getElementById("start-btn");
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn")
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question")
+var answerEl = document.getElementById("answer-buttons")
+let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame);
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log("Started");
+
     startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide");
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
 
-function selectAnswer() {
+function showQuestion(question){
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerEl.appendChild(button)
+    })
 
 }
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while ( answerEl.firstChild)
+    answerEl.removeChild(answerEl.firstChild)
+
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass( document.body, correct)
+    Array.from(answerEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove("hide")
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("corrrect")
+    element.classList.remove("wrong")
+}
+
+var questions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: [
+            { text: "strings", correct: false},
+            {text: "booleans", correct: false},
+            {text: "alerts", correct: true}, 
+            {text: "numbers", correct: false},
+        ]
+    
+    
+         
+        question: "The condition of an if/else statement is enclosed with:",
+        answers: [
+            {text: "quotes", correct:false},
+            {text:"curly brackets", correct:true},
+            {text:"parenthesis", correct:false},
+            {text:"square brackets", correct:false},
+        ]
+    }
+]
