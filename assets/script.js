cff49e5dc832c8eaecd83782705b4c42cece8d37
Your Name <you@example.com>
2022-08-25 12:03:40 -0500

    Added questions but issue with last question

--- assets/script.js
+++ assets/script.js
@@ -1,98 +1,128 @@
 var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn")
+var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
-var questionEl = document.getElementById("question")
-var answerEl = document.getElementById("answer-buttons")
-let shuffledQuestions, currentQuestionIndex
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answer-buttons");
 
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
+    currentQuestionIndex++;
+    setNextQuestion();
+});
 
 function startGame() {
-
     startButton.classList.add("hide");
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContainerEl.classList.remove("hide");
-    setNextQuestion()
+    setNextQuestion();
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-function showQuestion(question){
-    questionEl.innerText = question.question
+function showQuestion(question) {
+    questionEl.innerText = question.question;
     question.answers.forEach(answer => {
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
         if (answer.correct) {
-            button.dataset.correct = answer.correct
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click", selectAnswer)
-        answerEl.appendChild(button)
+        button.addEventListener("click", selectAnswer);
+        answerButtonsEl.appendChild(button);
     })
-
 }
 
 function resetState() {
-    nextButton.classList.add("hide")
-    while ( answerEl.firstChild)
-    answerEl.removeChild(answerEl.firstChild)
-
+    clearStatusClass(document.body);
+    nextButton.classList.add("hide");
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+    }
 }
 
 function selectAnswer(e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass( document.body, correct)
-    Array.from(answerEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    nextButton.classList.remove("hide")
+    var selectedButton = e.target;
+    var correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide");
+    } else {
+        startButton.innerText = "Log Results";
+        startButton.classList.remove("hide");
+    }
+
 }
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
+    clearStatusClass(element);
     if (correct) {
-        element.classList.add("correct")
+        element.classList.add("correct");
     } else {
-        element.classList.add("wrong")
+        element.classList.add("wrong");
     }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove("corrrect")
-    element.classList.remove("wrong")
+    element.classList.remove("corrrect");
+    element.classList.remove("wrong");
 }
 
-var questions = [
+const questions = [
     {
         question: "Commonly used data types do NOT include:",
         answers: [
-            { text: "strings", correct: false},
-            {text: "booleans", correct: false},
-            {text: "alerts", correct: true}, 
-            {text: "numbers", correct: false},
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false} ,
+            { text: "alerts", correct: true }, 
+            { text: "numbers", correct: false }
         ]
-    }
-        
-    /*{
-        question: "Hahahahaha",
-        answers:[
-            {text: "ha", correct:false},
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with:",
+        answers: [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: true },
+            { text: "parenthesis", correct: false },
+            { text: "square brackets", correct: false } 
 
         ]
+    },
+    {
+        question: "Arrays in Javsacript can be used to store:",
+        answers: [
+            { text: "numbers and strings", correct: false },
+            { text: "other Arrays", correct: false },
+            { text: "booleans", correct: false },
+            { text: "All of the above", correct: true }
+        ]
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answers:[
+            { text: "commas", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "quotes", correct: true },
+            { text: "parenthesis", correct: false }
+
+        ]
+    },
+    {
+        question: "A very useful tool during development and debugging for printing content to the debugger is:",
+        answer: [
+            { text: "Javascript", correct: false },
+            { text: "terminal/bash", correct: false },
+            { text: "for loops", correct: false },
+            { text: "console.log", correct: true }
+        ]
     }
-    
-         
-       
-    
-]
+];
